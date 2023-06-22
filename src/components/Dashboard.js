fd63a1ff5d07b129ea8fb83fbe1dcb1fc479b67a
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-06-23 00:40:30 +0530

    initial

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-
 function Dashboard({ setLoggedIn }) {
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
 
-    return (
-        <div id="dashboardPage">
-            <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
-        </div>
-    )
-
+  return (
+    <div id="dashboardPage">
+      <h1 id="dashboard-tag">Welcome to Dashboard</h1>
+      <button id="dashboard-logout" onClick={handleLogout}>Log out</button>
+    </div>
+  );
 }
+
 export default Dashboard;
